7f260158226060689880c3002ec213f06ad9c86d
tha_nguyen <>
2026-03-02 17:42:43 +0700

    fix staff

--- src/routes/staffOrders.js
+++ src/routes/staffOrders.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { authorize } from "../middleware/authorize.js";
 import db from "../db.js";
 
 const router = express.Router();
