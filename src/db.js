b78aada910e3d1edc4d654c24f7f956447841a6b
tha_nguyen <>
2026-03-15 15:15:24 +0700

    fix test db

--- src/db.js
+++ src/db.js
@@ -7,3 +7,7 @@ export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
+
+pool.connect()
+  .then(() => console.log("✅ DB connected"))
+  .catch(err => console.error("❌ DB connection error:", err));
