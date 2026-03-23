d2ffcada10b2c41b854529fb9197bf44074d9063
tha_nguyen <>
2026-03-23 15:16:16 +0700

    fix start server

--- src/db.js
+++ src/db.js
@@ -6,7 +6,9 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 
   // tối đa connection
   max: 10,
